2a3226d9e0ee123036c083cdcf7e752c270cb364
Aleksander Maj <aaarys@gmail.com>
2020-04-21 17:41:44 +0200

    Configure mutliple sources

--- src/App.svelte
+++ src/App.svelte
@@ -3,30 +3,34 @@
 	import { scaleLinear } from 'd3-scale';
 	import {data1, data2} from './data.js';
 
-	const yTicks = [0, 2, 4, 6, 8];
+	// INITIALIZE
 	const xTicks = [1980, 1990, 2000, 2010];
 	const padding = { top: 20, right: 15, bottom: 20, left: 25 };
 
 	let width = 500;
 	let height = 200;
 
-	const points = data1
+	let points = data1
 
-	const initalPoints = points.map((point) => ({ ...point, y: 0}));
-	const springPoints = spring(initalPoints, {
+	let xScale = scaleLinear()
+			.domain([1979, 2016])
+			.range([padding.left, width - padding.right]);
+
+	let maxY = Math.max.apply(null, points.map(p => p.y))
+	let yTicksInit = [0, 0.4 * maxY, 0.8 * maxY, maxY * 1.2]
+	let yScale = scaleLinear()
+			.domain([Math.min.apply(null, yTicksInit), Math.max.apply(null, yTicksInit)])
+			.range([height - padding.bottom, padding.top]);
+
+	const initialPoints = points.map((point) => ({ x: xScale(point.x), y: yScale(0) }));
+	const springPoints = spring(initialPoints, {
 		stiffness: 0.1,
 		damping: 0.9,
 		precision: 0.1
 	})
 
-	setTimeout(() => {
-		springPoints.set(data1)
-	}, 100)
-
-
-	setTimeout(() => {
-		springPoints.set(data2)
-	}, 1000)
+	$: maxY = Math.max.apply(null, points.map(p => p.y))
+	$: yTicks = [0, 0.4 * maxY, 0.8 * maxY, maxY * 1.2]
 
 	$: xScale = scaleLinear()
 		.domain([minX, maxX])
@@ -36,15 +40,35 @@
 		.domain([Math.min.apply(null, yTicks), Math.max.apply(null, yTicks)])
 		.range([height - padding.bottom, padding.top]);
 
-	
-
 	$: minX = points[0].x;
 	$: maxX = points[points.length - 1].x;
-	$: path = `M${$springPoints.map(p => `${xScale(p.x)},${yScale(p.y)}`).join('L')}`;
+	$: path = `M${$springPoints.map(p => `${p.x},${p.y}`).join('L')}`;
 	$: area = `${path}L${xScale(maxX)},${yScale(0)}L${xScale(minX)},${yScale(0)}Z`;
+
+	setTimeout(() => {
+		const scaledPoints = data1.map(p => ({ x: xScale(p.x), y: yScale(p.y) }))
+		springPoints.set(scaledPoints)
+	}, 1000)
 
 	function formatMobile(tick) {
 		return "'" + tick % 100;
+	}
+
+	function setData1() {
+		points = data1
+
+		setTimeout(() => {
+			const scaledPoints = data1.map(p => ({ x: xScale(p.x), y: yScale(p.y) }))
+			springPoints.set(scaledPoints)
+		}, 0)
+	}
+
+	function setData2() {
+		points = data2
+		setTimeout(() => {
+			const scaledPoints = data2.map(p => ({ x: xScale(p.x), y: yScale(p.y) }))
+			springPoints.set(scaledPoints)
+		}, 0)
 	}
 </script>
 
@@ -71,10 +95,13 @@
 		</g>
 
 		<!-- data -->
-		<!-- <path class="path-area" d={area}></path> -->
+		<path class="path-area" d={area}></path>
 		<path class="path-line" d={path}></path>
 	</svg>
 </div>
+
+<button on:click={setData1}>Dataset 1</button>
+<button on:click={setData2}>Dataset 2</button>
 
 <style>
 	.chart, h2, p {
