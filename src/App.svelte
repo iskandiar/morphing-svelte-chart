90ac462249d1df0cbbd8cf8211eaf2e269f37e4b
Aleksander Maj <aaarys@gmail.com>
2020-04-23 14:52:10 +0200

    Add stock prices for Q1 for top5 tech companies and make it works

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,40 @@
 <script>
 	import { spring } from 'svelte/motion';
 	import { scaleLinear } from 'd3-scale';
-	import {data1, data2} from './data.js';
+	import { amazon, apple, facebook, tesla, microsoft } from './data.js';
+
+	const dataSets = {
+		amazon: {
+			data: amazon
+		},
+		apple: {
+			data: apple
+		},
+		facebook: {
+			data: facebook
+		}, 
+		tesla: {
+			data: tesla
+		},
+		microsoft: {
+			data: microsoft
+		}
+	}
 
 	// INITIALIZE
 	const xTicks = [1980, 1990, 2000, 2010];
 	const padding = { top: 20, right: 15, bottom: 20, left: 25 };
 
-	let width = 500;
-	let height = 200;
+	let width = 600;
+	let height = 300;
 
-	let points = data1
+	let points = amazon
+	let data = amazon
 
+	let minX = points[0].x;
+	let maxX = points[points.length - 1].x;
 	let xScale = scaleLinear()
-			.domain([1979, 2016])
+			.domain([new Date(minX), new Date(maxX)])
 			.range([padding.left, width - padding.right]);
 
 	let maxY = Math.max.apply(null, points.map(p => p.y))
@@ -22,7 +43,7 @@
 			.domain([Math.min.apply(null, yTicksInit), Math.max.apply(null, yTicksInit)])
 			.range([height - padding.bottom, padding.top]);
 
-	const initialPoints = points.map((point) => ({ x: xScale(point.x), y: yScale(0) }));
+	const initialPoints = points.map((point) => ({ x: xScale(new Date(point.x)), y: yScale(0) }));
 	const springPoints = spring(initialPoints, {
 		stiffness: 0.1,
 		damping: 0.9,
@@ -33,7 +54,7 @@
 	$: yTicks = [0, 0.4 * maxY, 0.8 * maxY, maxY * 1.2]
 
 	$: xScale = scaleLinear()
-		.domain([minX, maxX])
+		.domain([new Date(minX), new Date(maxX)])
 		.range([padding.left, width - padding.right]);
 
 	$: yScale = scaleLinear()
@@ -43,30 +64,22 @@
 	$: minX = points[0].x;
 	$: maxX = points[points.length - 1].x;
 	$: path = `M${$springPoints.map(p => `${p.x},${p.y}`).join('L')}`;
-	$: area = `${path}L${xScale(maxX)},${yScale(0)}L${xScale(minX)},${yScale(0)}Z`;
+	$: area = `${path}L${xScale(new Date(maxX))},${yScale(0)}L${xScale(new Date(minX))},${yScale(0)}Z`;
 
 	setTimeout(() => {
-		const scaledPoints = data1.map(p => ({ x: xScale(p.x), y: yScale(p.y) }))
+		const scaledPoints = data.map(p => ({ x: xScale(new Date(p.x)), y: yScale(p.y) }))
 		springPoints.set(scaledPoints)
-	}, 1000)
+	}, 200)
 
 	function formatMobile(tick) {
 		return "'" + tick % 100;
 	}
 
-	function setData1() {
-		points = data1
+	function setData(key) {
+		points = dataSets[key].data
 
 		setTimeout(() => {
-			const scaledPoints = data1.map(p => ({ x: xScale(p.x), y: yScale(p.y) }))
-			springPoints.set(scaledPoints)
-		}, 0)
-	}
-
-	function setData2() {
-		points = data2
-		setTimeout(() => {
-			const scaledPoints = data2.map(p => ({ x: xScale(p.x), y: yScale(p.y) }))
+			const scaledPoints = points.map(p => ({ x: xScale(new Date(p.x)), y: yScale(p.y) }))
 			springPoints.set(scaledPoints)
 		}, 0)
 	}
@@ -100,13 +113,14 @@
 	</svg>
 </div>
 
-<button on:click={setData1}>Dataset 1</button>
-<button on:click={setData2}>Dataset 2</button>
+{#each Object.keys(dataSets) as name, i}
+	<button on:click={() => setData(name)}> {name} </button>
+{/each}
 
 <style>
 	.chart, h2, p {
 		width: 100%;
-		max-width: 500px;
+		max-width: 600px;
 		margin-left: auto;
 		margin-right: auto;
 	}
@@ -114,7 +128,7 @@
 	svg {
 		position: relative;
 		width: 100%;
-		height: 200px;
+		height: 300px;
 		overflow: visible;
 	}
 
@@ -153,3 +167,13 @@
 		fill: rgba(0,100,100,0.2);
 	}
 </style>
+
+
+<!-- TO-DO
+- vertical slider
+- extarct to components
+- different colors for datasets
+- morphing between colors
+- nice value idicator
+- better dataset (exchange?)
+-  -->
